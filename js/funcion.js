5d1677985d22fa2ed07760f08bce79b60e59edb2
Ezequiel García <ezeezegg@gmail.com>
2014-11-20 07:56:53 -0600

    efren
    efren

---
+++ js/funcion.js
@@ -0,0 +1,36 @@
+function carga(){
+	$(".content").fadeIn();
+	$(".producto").fadeOut();
+	$(".cabla").fadeOut();
+	$(".vitin").fadeOut();
+	$(".aceite").fadeOut();
+}
+
+function muestrac(){
+	$(".content").fadeOut();
+	$(".producto").fadeIn();
+	$(".vitin").fadeOut();
+	$(".aceite").fadeOut();
+	$(".cabla").fadeIn();
+}
+
+function muestrav(){
+	$(".content").fadeOut();
+	$(".producto").fadeIn();
+	$(".cabla").fadeOut();
+	$(".aceite").fadeOut();
+	$(".vitin").fadeIn();
+}
+
+function muestraa(){
+	$(".content").fadeOut();
+	$(".producto").fadeIn();
+	$(".cabla").fadeOut();
+	$(".vitin").fadeOut();
+	$(".aceite").fadeIn();
+}
+
+function muestrai(){
+	$(".producto").fadeOut();
+	$(".content").fadeIn();
+}
